9d751386d5e72f51d5553ea6900566e4cf5a2d1b
Fauzaro01 <muhamadfauzan4750@gmail.com>
2023-08-15 00:24:46 +0700

    Add auth routers for next commit

--- app.js
+++ app.js
@@ -22,6 +22,8 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser())
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use('/auth', require('./routes/index'))
+
 app.get('/', (req, res) => {
     res.status(200).render('index', {
         layout: 'layout/main',
